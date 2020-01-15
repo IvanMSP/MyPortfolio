5e36a6a06593d986418e61e849aa5af85844f141
Ivan López <ivanlopz11@gmail.com>
2020-01-14 23:19:38 -0600

    added urls base to portfolio

---
+++ apps/portfolio/api/v1/urls.py
@@ -0,0 +1,8 @@
+# Django Core Libraries
+from django.urls import path
+from .views import *
+
+
+portfolio_v1_urls = [
+    # path('profile/', ProductionListView.as_view(), name='profile'),
+]
---
+++ apps/portfolio/urls.py
@@ -0,0 +1,3 @@
+from .api.v1.urls import portfolio_v1_urls
+
+portfolio_urls = portfolio_v1_urls
