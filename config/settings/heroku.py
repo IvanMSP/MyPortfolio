052736e446479d181167a2e74ef925497c2ccd3f
Ivan López <ivanlopz11@gmail.com>
2020-01-31 07:09:11 -0600

    requirements.txt

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,11 +1,10 @@
 from .base import *
-
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
 import dj_database_url
 from decouple import config
+
+DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
+
+ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': dj_database_url.config(
@@ -22,4 +21,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-SECRET_KEY = '3sc8v@mxzfssp=vfbh!k=@4t!u9lwlgqx&*^o%v@l3$9z)-le)'
+SECRET_KEY = config('SECRET_KEY', default='3sc8v@mxzfssp=vfbh!k=@4t!u9lwlgqx&*^o%v@l3$9z)-le)')
+
+if config('DJANGO_PRODUCTION_ENV', default=False, cast=bool):
+    from .base import *
